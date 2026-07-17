4365bc845e09af076112f20a50854c98cb36e6e8
Ekane <ekanebryan618@gmail.com>
2026-07-17 08:51:17 +0100

    environemt fixes

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -17,6 +17,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:5174',
+        env('FRONTEND_URL'),
     ],
 
     'allowed_origins_patterns' => [],
