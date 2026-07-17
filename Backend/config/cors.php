e344a3479be1ccdb6757ea93049aa8aa3441e956
Ekane <ekanebryan618@gmail.com>
2026-07-17 08:57:48 +0100

    allowed origins link fro frontend fixed

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -17,7 +17,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://vercel.app',
+        'https://mboaevents237.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
