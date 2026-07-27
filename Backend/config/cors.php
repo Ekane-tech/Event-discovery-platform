54fd8a626710e35e843cee6199c29380e592adc2
ekane bryan <ekanebryan5@gmail.com>
2026-07-27 17:17:55 +0100

    fix: frontend url added to cors

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -13,7 +13,7 @@ return [
     'allowed_methods' => ['*'],
 
     // SÉCURITÉ : Gardez vos localhost ET votre domaine Vercel en dur si la variable env a un raté
-        'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://localhost:5174,https://mboaevents237.vercel.app','mboa-events-platform-staging.up.railway.app')))),
+        'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://localhost:5174,https://mboaevents237.vercel.app,https://event-discovery-platform-git-staging-ekane-bryan-s-projects.vercel.app')))),
 
     'allowed_origins_patterns' => [],
 
